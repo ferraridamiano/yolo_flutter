ead74fafdebfd128720eee8a8895fdb2b19a9ecb
Damiano Ferrari <damianoferrari1998@gmail.com>
2025-05-03 15:50:50 +0200

    chore: re-generate android folder

--- android/app/src/main/kotlin/com/example/yolo_flutter/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.yolo_flutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ android/app/src/main/kotlin/io/github/ferraridamiano/yolo_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package io.github.ferraridamiano.yolo_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
